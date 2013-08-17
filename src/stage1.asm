b721cf40b40dc7178e2483e232cfce2247da1c20
Skylar Kelty <skylarkelty@gmail.com>
2013-08-17 17:00:04 +0100

    Remove param block

--- src/stage1.asm
+++ src/stage1.asm
@@ -4,27 +4,6 @@ jmp 07C0h:start
 
 start: jmp load
 
-; Parameter block
-bpbOEM                  DB "Sparse OS"
-bpbBytesPerSector:      DW 512
-bpbSectorsPerCluster:   DB 1
-bpbReservedSectors:     DW 1
-bpbNumberOfFATs:        DB 2
-bpbRootEntries:         DW 224
-bpbTotalSectors:        DW 2880
-bpbMedia:               DB 0xF0
-bpbSectorsPerFAT:       DW 9
-bpbSectorsPerTrack:     DW 18
-bpbHeadsPerCylinder:    DW 2
-bpbHiddenSectors:       DD 0
-bpbTotalSectorsBig:     DD 0
-bsDriveNumber:          DB 0
-bsUnused:               DB 0
-bsExtBootSignature:     DB 0x29
-bsSerialNumber:         DD 0xa0a1a2a3
-bsVolumeLabel:          DB "MOS FLOPPY"
-bsFileSystem:           DB "FAT12"
-
 ; Load our text
 text_string db 'Welcome to Sparse!', 0
 
