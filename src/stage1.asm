2c41730a57a494fb8e0802c4a1d47acab5a3c5b2
Skylar Kelty <skylarkelty@gmail.com>
2013-08-17 16:51:48 +0100

    Add parameter block

--- src/stage1.asm
+++ src/stage1.asm
@@ -2,8 +2,31 @@ ORG 0
 BITS 16
 jmp 07C0h:start
 
+start: jmp load
+
+; Parameter block
+times 0Bh-$+start DB 0
+bpbBytesPerSector:      DW 512
+bpbSectorsPerCluster:   DB 1
+bpbReservedSectors:     DW 1
+bpbNumberOfFATs:        DB 2
+bpbRootEntries:         DW 224
+bpbTotalSectors:        DW 2880
+bpbMedia:               DB 0xF0
+bpbSectorsPerFAT:       DW 9
+bpbSectorsPerTrack:     DW 18
+bpbHeadsPerCylinder:    DW 2
+bpbHiddenSectors:       DD 0
+bpbTotalSectorsBig:     DD 0
+bsDriveNumber:          DB 0
+bsUnused:               DB 0
+bsExtBootSignature:     DB 0x29
+bsSerialNumber:         DD 0xa0a1a2a3
+bsVolumeLabel:          DB "MOS FLOPPY"
+bsFileSystem:           DB "FAT12"
+
 ; Begin load
-start:
+load:
     ; Set up 4K stack space after this bootloader
     ; (4096 + 512) / 16 bytes per paragraph
     mov ax, 07C0h
